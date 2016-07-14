f8da87e0cdc37ca2a653f0a754a8f094a1a16b41
Ziyi Yan <cxfyzy@gmail.com>
2016-07-14 15:26:56 +0800

    Completed exercise 3.13

---
+++ ch3/ex3.13.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+const (
+	KB = 1000
+	MB = KB * KB
+	GB = MB * KB
+	TB = GB * KB
+	PB = TB * KB
+	EB = PB * KB
+	ZB = EB * KB
+	YB = ZB * KB
+)
+
+func main() {
+	fmt.Println("KB:\t", KB)
+	fmt.Println("MB:\t", MB)
+	fmt.Println("GB:\t", GB)
+	fmt.Println("TB:\t", TB)
+	fmt.Println("PB:\t", PB)
+	fmt.Println("EB:\t", EB)
+	fmt.Println("YB/ZB:\t", YB/ZB)
+	//fmt.Println(ZB, YB) // Warning: constant ... overflows int
+}
